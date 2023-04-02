b09656fcc22cd72196074ade59383d40c619fbfb
stayhomewelldone <0973013@hr.nl>
2023-04-02 18:19:14 +0200

    A lot of changes and additional code.

---
+++ predict.js
@@ -0,0 +1,44 @@
+let options;
+let nn;
+let input;
+let result = document.getElementById("result");
+let predictButton = document.getElementById("btn");
+let euroValue;
+
+predictButton.addEventListener("click", () => {
+  const zipcodeValue = parseInt(document.getElementById("zipcode").value);
+  const buildyearValue = parseInt(document.getElementById("buildyear").value);
+  const bathroomsValue = parseInt(document.getElementById("bathrooms").value);
+
+  makePrediction(zipcodeValue, buildyearValue, bathroomsValue);
+});
+
+options = {
+  task: "regression",
+  debug: true,
+};
+nn = ml5.neuralNetwork(options);
+nn.load("./model/model.json", modelLoaded);
+
+function modelLoaded() {
+  console.log("Model loaded!");
+}
+
+async function makePrediction(zipcode, buildyear, bathrooms) {
+  input = {
+    zipcode: zipcode,
+    buildyear: buildyear,
+    bathrooms: bathrooms,
+  };
+
+  const pred = await nn.predict(input);
+  const fmt = new Intl.NumberFormat("nl-NL", {
+    style: "currency",
+    currency: "EUR",
+  });
+  euroValue = fmt.format(pred[0].retailvalue);
+  //   //   console.log(fmt.format(pred));
+  //   console.log(pred);
+  console.log(euroValue);
+  result.innerHTML += euroValue;
+}
---
+++ train.js
@@ -0,0 +1,78 @@
+let nn;
+let inputs;
+let output;
+let inputHouse;
+let testData;
+
+let save = document.getElementById("btn");
+
+save.addEventListener("click", () => {
+  nn.save(modelSaved());
+});
+
+function loadData() {
+  Papa.parse("./data/utrecht-houseprices.csv", {
+    download: true,
+    header: true,
+    dynamicTyping: true,
+    complete: (results) => checkData(results.data),
+  });
+}
+
+function checkData(data) {
+  console.log("Data loaded!");
+  // data voorbereiden
+  data.sort(() => Math.random() - 0.5);
+  let trainData = data.slice(0, Math.floor(data.length * 0.8));
+  testData = data.slice(Math.floor(data.length * 0.8) + 1);
+
+  const options = {
+    task: "regression",
+    debug: true,
+  };
+
+  // neural network aanmaken
+  nn = ml5.neuralNetwork(options);
+
+  // data toevoegen aan neural network
+  data.forEach((item) => {
+    inputs = {
+      zipcode: item.Zipcode,
+      buildyear: item.Buildyear,
+      bathrooms: item.bathrooms,
+    };
+    output = {
+      retailvalue: item.retailvalue,
+    };
+    nn.addData(inputs, output);
+  });
+
+  nn.normalizeData();
+  trainModel();
+}
+
+function trainModel() {
+  const trainingOptions = {
+    epochs: 30,
+  };
+  nn.train(trainingOptions, finishedTraining);
+}
+function finishedTraining() {
+  console.log("Finished training!");
+  makePrediction();
+}
+
+async function makePrediction() {
+  inputHouse = {
+    zipcode: testData[0].Zipcode,
+    buildyear: testData[0].Buildyear,
+    bathrooms: testData[0].bathrooms,
+  };
+  const pred = await nn.predict(inputHouse);
+  console.log(parseInt(pred[0].retailvalue));
+}
+function modelSaved() {
+  console.log("Model saved!");
+}
+
+loadData();
